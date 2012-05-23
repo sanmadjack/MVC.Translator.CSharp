0e8123640b40bbc5ef55945f5ae1775b101ad049
ramanan padmanabhlan <ramanan.padmanabhlan@kohls.com>
2012-05-23 13:29:40 -0500

    Initial commit

---
+++ TranslatingProgressHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Translator;
+namespace Communication.Translator {
+    public class TranslatingProgressHandler : ProgressHandler {
+        public static void setTranslatedMessage(string name, params string[] variables) {
+            String line = Strings.getString(StringType.Message, name, variables);
+            ProgressHandler.message = line;
+
+        }
+    }
+}
---
+++ TranslatingRequestHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Translator;
+namespace Communication.Translator {
+    public class TranslatingRequestHandler : RequestHandler {
+
+        public static RequestReply Request(RequestType type, string name, params string[] variables) {
+
+            return TranslatingRequestHandler.Request(type, name, null, variables);
+        }
+
+        public static RequestReply Request(RequestType type, string name, List<string> choices, params string[] variables) {
+            return TranslatingRequestHandler.Request(type, name, null, choices, variables);
+        }
+
+        public static RequestReply Request(RequestType type, string name, string default_choice, List<string> choices, params string[] variables) {
+            StringCollection col = Strings.getStrings(name);
+
+            return RequestHandler.Request(type, col[StringType.Title].interpret(), col[StringType.Message].interpret(variables), choices, default_choice);
+
+        }
+    }
+}
