30017ea6d0ef4bbc1d83a93180acb9111f4b0972
Matthew Barbour <sanmadjack@gmail.com>
2012-06-05 13:01:29 -0500

    Added exception if progress message is sent using a string that isn't found

--- TranslatingProgressHandler.cs
+++ TranslatingProgressHandler.cs
@@ -4,6 +4,9 @@ namespace Communication.Translator {
     public class TranslatingProgressHandler : ProgressHandler {
         public static void setTranslatedMessage(string name, params string[] variables) {
             String line = Strings.getString(StringType.Message, name, variables);
+            if (line == name) {
+                throw new StringNotFoundException(name, StringType.Message);
+            }
             ProgressHandler.message = line;
 
         }
